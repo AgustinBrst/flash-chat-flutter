e499d10d5c261d9e2ae0ed0ab48115bf263141ed
Agustin Burset <agustin.burset@icloud.com>
2020-01-06 13:54:37 -0300

    Add named routes for each screen, with the welcome screen as default

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: '/welcome',
+      routes: {
+        '/welcome': (_) => WelcomeScreen(),
+        '/login': (_) => LoginScreen(),
+        '/registration': (_) => RegistrationScreen(),
+        '/chat': (_) => ChatScreen(),
+      },
     );
   }
 }
