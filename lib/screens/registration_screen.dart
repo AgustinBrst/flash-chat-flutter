96943e76b393cc9187cae2cafd1c0a469def2d0a
Agustin Burset <agustin.burset@icloud.com>
2020-01-08 09:45:54 -0300

    Add loading spinner on login and registration screens, fix imports

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,8 +1,9 @@
-import 'package:flash_chat/routing/routes.dart';
-import 'package:flash_chat/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import '../constants.dart';
+import '../routing/routes.dart';
+import '../services/auth_service.dart';
 import '../widgets/button.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -14,57 +15,65 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final AuthService authService = AuthService();
   String email;
   String password;
+  bool inAsyncCall = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: inAsyncCall,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) => email = value,
-              keyboardType: TextInputType.emailAddress,
-              decoration: textFieldDecoration.copyWith(hintText: 'Enter your email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) => password = value,
-              obscureText: true,
-              decoration: textFieldDecoration.copyWith(hintText: 'Enter your password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Button(
-                text: 'Register',
-                color: Colors.blueAccent,
-                onPressed: () async {
-                  final newUser = await authService.createUserWithEmailAndPassword(email: email, password: password);
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, Routes.chat);
-                  }
-                },
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-          ],
+              TextField(
+                onChanged: (value) => email = value,
+                keyboardType: TextInputType.emailAddress,
+                decoration: textFieldDecoration.copyWith(hintText: 'Enter your email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) => password = value,
+                obscureText: true,
+                decoration: textFieldDecoration.copyWith(hintText: 'Enter your password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Button(
+                  text: 'Register',
+                  color: Colors.blueAccent,
+                  onPressed: () async {
+                    setState(() => inAsyncCall = true);
+
+                    final newUser = await authService.createUserWithEmailAndPassword(email: email, password: password);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, Routes.chat);
+                    }
+
+                    setState(() => inAsyncCall = false);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
